00eb11e0643ae6463f1a09ef9c8a631495263569
Alex Markessinis <markea125@gmail.com>
2017-12-09 00:19:31 -0500

    updated to clean up logs

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'markea125@gmail.com'
 license 'MIT'
 description 'Configures a Keepalived cluster with shared virtual IP addresses.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 %w( centos debian redhat ubuntu ).each do |os|
   supports os
 end
